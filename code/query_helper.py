9185b9f1cbbb3705014d0e0281e668d47f37acbf
pratheeksh <pks329@nyu.edu>
2017-04-22 13:29:38 -0400

    tested query

--- code/query_helper.py
+++ code/query_helper.py
@@ -1,10 +1,9 @@
-from scipy.spatial import KDTree
-
+import inventory
 from util.image_processing_fns import *
 from util.utils import *
-import inventory
+
 IM_RESIZE_DIMS = (227, 227)
-kd_tree_base = "/Users/pratheeksha/School/SEA-Project/data/test/trees"
+kd_tree_base = "/Users/pratheeksha/School/SEA-Project/data/test/features"
 
 model = load_model()
 
@@ -21,35 +20,31 @@ def process_input(image):
 
 class Query:
     def __init__(self, shard_id):
-        self.kd_tree = pickle.load(open(kd_tree_base + "/" + str(shard_id) + ".p", "rb"))
-        self.feat_vecs = np.load(open(kd_tree_base + "/" + str(shard_id) + ".npy", "rb"))
-        self.file_names = np.load(open(kd_tree_base + "/" + str(shard_id) + ".fname", "rb"))
+        self.kd_tree_dict = pickle.load(open(kd_tree_base + "/" + str(shard_id) + ".out", "rb"))
+        self.file_names = list(self.kd_tree_dict.keys())[0].split()
+        self.kd_tree = self.kd_tree_dict[list(self.kd_tree_dict.keys())[0]]
 
     def get_knn_image_feats(self, feature_vector):
         scores, keys = self.kd_tree.query(feature_vector, k=10)
-        top_k = {self.file_names[i]: self.feat_vecs[i] for i in keys}
-        return top_k
+        # top_k = {self.file_names[i]: self.feat_vecs[i] for i in keys}
+        return scores, keys
 
 
 # Whatever is under main will need to
 # move to start.py in the get section
 def main():
-    top_k_feats = []
-    top_k_keys = []
+    top_k_scores = []
     query_instances = [Query(i) for i in range(inventory.NUM_KD_TREES)]
     feat = process_input(getImage("11.jpg", "/Users/pratheeksha/School/SEA-Project/data/test/images/"))
 
     for query in query_instances:
-        top_k = query.get_knn_image_feats(feat)
-        for k in top_k:
-            top_k_keys.append(k)
-            top_k_feats.append(top_k[k])
+        scores, keys = query.get_knn_image_feats(feat)
+        for i in range(len(scores)):
+            top_k_scores.append((scores[i], query.file_names[keys[i]]))
 
-    T = KDTree(np.array(top_k_feats))
-    scores, keys = T.query(feat, k=10)
-    for k in keys:
-        print(top_k_keys[k])
-
+    results = sorted(top_k_scores)
+    for v, k in results[:10]:
+        print(k, v)
 
 if __name__ == '__main__':
     main()
