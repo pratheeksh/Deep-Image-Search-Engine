dcadfba415da23a8a76e2f05f25e47237157183d
Bharathi Priyaa <bt978@nyu.edu>
2017-04-27 14:51:34 -0400

    Adding updated js

--- code/webapp/static/main.js
+++ code/webapp/static/main.js
@@ -1,15 +1,25 @@
 // ----- custom js ----- //
 
 // sanity check
- console.log( "ready!" );
+console.log( "ready!" );
+$("#searching").hide();
+$("#results-table").hide();
+$("#error").hide();
+
 
 $(function() {
 
   // sanity check
-  console.log( "ready!" );
-
+  console.log( "ready123!" );
+  // remove active class
+  $(".img").removeClass("active")
   // image click
   $(".img").click(function() {
+
+    // empty/hide results
+    $("#results").empty();
+    $("#results-table").hide();
+    $("#error").hide();
 
     // add active class to clicked picture
     $(this).addClass("active")
@@ -18,17 +28,35 @@ $(function() {
     var image = $(this).attr("src")
     console.log(image)
 
+    // show searching text
+    $("#searching").show();
+    console.log("searching...")
+
+
     // ajax request
     $.ajax({
-      type: "POST",
+      type: "GET",
       url: "/search",
       data : { img : image },
       // handle success
-      success: function(result) {
-        console.log(result.results);
+      success: function(jsonResponse) {
+        console.log("Success something received")
+        var obj = JSON.parse(jsonResponse);
+        var data = obj.results
+        // show table
+        $("#results-table").show();
+        // loop through results, append to dom
+        for (i = 0; i < data.length; i++) {
+        $("#results").append('<tr><th><a href="'+data[i]["filename"]+'"><img src="'+data[i]["image_url"]+
+    '" class="result-img"></a></th><th>'+data[i]['text']+'</th></tr>')
+       };
+
+
+
       },
       // handle error
       error: function(error) {
+
         console.log(error);
       }
     });
