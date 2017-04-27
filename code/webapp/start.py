6e61b7e3f1d8dd7507ed5a3bb0749e3e4abad62a
pratheeksh <pks329@nyu.edu>
2017-04-27 17:36:20 -0400

    moved feature extractor to start so that each indexer neednt extract feature

--- code/webapp/start.py
+++ code/webapp/start.py
@@ -1,24 +1,49 @@
-import tornado, pickle, logging, urllib
-from tornado.ioloop import IOLoop
-from tornado import web, gen, process, httpserver, httpclient, netutil
-from . import inventory, index, doc
-from itertools import chain
+import json
+import logging
+import os
+import pickle
+import urllib
 from collections import defaultdict
-import json, sys, os, pickle
-from util.utils import load_model
+from itertools import chain
+
+import numpy as np
+import tornado
+from skimage import io as skimageio
+from skimage.transform import resize
+from tornado import web, gen, process, httpserver, httpclient, netutil
+from tornado.ioloop import IOLoop
+
+from util.utils import convert_array_to_Variable
+from . import inventory, index, doc
+
 NUM_RESULTS = 10
 
 SETTINGS = {
-            "debug": True,
-            "static_path": os.path.join(os.path.dirname(__file__), 'static/'),
-            "template_path": os.path.join(os.path.dirname(__file__), 'templates/')
-        }
+    "debug": True,
+    "static_path": os.path.join(os.path.dirname(__file__), 'static/'),
+    "template_path": os.path.join(os.path.dirname(__file__), 'templates/')
+}
 log = logging.getLogger(__name__)
 
 
 class Web(web.RequestHandler):
+    def initialize(self, model):
+        self.model = model
+
     def head(self):
         self.finish()
+
+    def get_feature_vector(self, image_url):
+
+        im = skimageio.imread(image_url)
+        print("Loaded image size", im.shape)
+        image = resize(im, inventory.IM_RESIZE_DIMS)
+        image = np.transpose(image, (2, 0, 1))
+        image = convert_array_to_Variable(np.array([image]))
+        # pass a batch with just the queried image to alexnet
+        feature_vector = self.model(image)
+        print("Generated feature vector of size {}".format(feature_vector.data.numpy().shape))
+        return feature_vector.data.numpy().reshape((4096,))
 
     @gen.coroutine
     def get(self):
@@ -27,15 +52,16 @@ class Web(web.RequestHandler):
         q = self.get_argument('img', None)
         if q is None:
             return
-
+        feature_vector = self.get_feature_vector(q)
         # Fetch postings from index servers
         http = httpclient.AsyncHTTPClient()
-        responses = yield [http.fetch('http://%s/index?%s' % (server, urllib.parse.urlencode({'q': q})))
+
+        responses = yield [http.fetch('http://%s/index?%s' % (server, urllib.parse.urlencode({'q': feature_vector})))
                            for server in inventory.servers['index']]
         # Flatten postings and sort by score
         postings = sorted(chain(*[json.loads(r.body.decode())['postings'] for r in responses]),
                           key=lambda x: -x[0])[:NUM_RESULTS]
-        #postings have the format {"postings": [[53.61725232526324, "285.jpg"]} score, id
+        # postings have the format {"postings": [[53.61725232526324, "285.jpg"]} score, id
         print(postings)
         # Batch requests to doc servers
         server_to_doc_ids = defaultdict(list)
@@ -53,7 +79,6 @@ class Web(web.RequestHandler):
                 result_list[doc_id_to_result_ix[int(result['doc_id'])]] = result
         print("Finished retrieving documents", result_list)
         self.write(json.dumps({'num_results': len(result_list), 'results': result_list}))
-
 
     def _get_doc_server_futures(self, q, server_to_doc_ids):
         http = httpclient.AsyncHTTPClient()
@@ -79,11 +104,12 @@ def main():
     num_procs = inventory.NUM_INDEX_SHARDS + inventory.NUM_DOC_SHARDS + 1
     task_id = process.fork_processes(num_procs, max_restarts=0)
     port = inventory.BASE_PORT + task_id
-    if task_id == 0 : 
+    if task_id == 0:
         app = httpserver.HTTPServer(tornado.web.Application([
-                (r'/search', Web),
-                (r'/(.*)', tornado.web.StaticFileHandler, {"path" : SETTINGS["template_path"], "default_filename": "index.html"})
-            ], **SETTINGS))
+            (r'/search', Web),
+            (r'/(.*)', tornado.web.StaticFileHandler, {"path": SETTINGS["template_path"],
+                                                       "default_filename": "index.html"})
+        ], **SETTINGS))
         log.info('Front end is listening on %d', port)
     else:
         if task_id <= inventory.NUM_INDEX_SHARDS:
@@ -100,6 +126,7 @@ def main():
 
     app.add_sockets(netutil.bind_sockets(port))
     IOLoop.current().start()
+
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', level=logging.DEBUG)
