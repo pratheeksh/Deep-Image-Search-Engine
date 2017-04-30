463d0ba9f08a080a37f42c9d2ac9d3536953250e
pratheeksh <pks329@nyu.edu>
2017-04-30 21:31:20 +0000

    revert inventory

--- code/inventory.py
+++ code/inventory.py
@@ -1,14 +1,14 @@
 import getpass
 import hashlib
-
+#####  DO NOT TOUCH THE PORTS PLEASE ####
 MAX_PORT = 49123
-MIN_PORT = 25000
-HOSTNAME = "http://localhost"
-#HOSTNAME =  "http://ec2-54-200-53-153.us-west-2.compute.amazonaws.com"
+MIN_PORT = 10000
+#HOSTNAME = "http://localhost"
+HOSTNAME =  "http://ec2-54-200-53-153.us-west-2.compute.amazonaws.com"
 BASE_PORT = int(hashlib.md5(getpass.getuser().encode()).hexdigest()[:8], 16) % \
             (MAX_PORT - MIN_PORT) + MIN_PORT + 100
 
-NUM_INDEX_SERVERS = 10
+NUM_INDEX_SERVERS = 20
 NUM_TXT_INDEX_SERVERS = 10
 NUM_DOC_SERVERS = 10
 MAX_NUM_RESULTS = 30
@@ -20,15 +20,9 @@ TO_DISPLAY = 10
 TITLE_BONUS = 10.0
 WORKER_THREAD_COUNT = 8
 WORKER_PORTS = []
-
-DOCS_STORE = "data/biggertest/docs/docshard_%d.p"
-TREE_STORE = "data/biggertest/features"
-TEXT_STORE = "data/biggertest/indices"
-'''
 DOCS_STORE = "data/FlickrData2/docs/docshard_%d.p"
 TREE_STORE = "data/FlickrData2/features"
 TEXT_STORE = "data/FlickrData2/indices"
-'''''
 IM_RESIZE_DIMS = (227, 227)
 # NUM_KD_TREES = 3
 WEBAPP_PATH = "static/"
